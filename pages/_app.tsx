f4f82b5df534db7fba4769312494c4ed1d751867
Henok.T <123959158+henoktsegayetoptal@users.noreply.github.com>
2024-06-07 00:25:40 +0300

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,25 @@ import { NextUIProvider } from "@nextui-org/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { fontSans, fontMono } from "@/config/fonts";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <NextUIProvider>
       <NextThemesProvider attribute="class" defaultTheme="dark">
+      <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-8LFFV6MN7W"
+        />
+
+        <Script id="google-analytics">
+          {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-8LFFV6MN7W');
+          `}
+        </Script>
         <Component {...pageProps} />
       </NextThemesProvider>
     </NextUIProvider>
